8737a0ab660adbcb0c4fd3d193a1ebf0e8b0e556
InnanenSanteri <157682130+InnanenSanteri@users.noreply.github.com>
2024-04-24 12:24:49 +0300

    Update kuitti.h
    hhhhhhhhhhhhhhhhhhhhhhh

--- bankautomat_exe/bankautomat/kuitti.h
+++ bankautomat_exe/bankautomat/kuitti.h
@@ -20,6 +20,23 @@ public:
 private:
     Ui::Kuitti *ui;
     QSoundEffect printtaus;
+
+    std::unordered_map<QString, std::vector<QString>> dictionary = {
+        {"Test", std::vector<QString>() = {"Test", "Testi", "Testa"}},
+        {"ATM NUMBER",std::vector<QString>() = {"ATM NUMBER", "PANKKIAUTOMAATTI NUMERO","ATM NUMMER"}},
+        {"CLOCK",std::vector<QString>() = {"CLOCK", "KELLO", "KLOCKA"}},
+        {"CUSTOMER",std::vector<QString>() = {"CUSTOMER", "ASIAKAS", "KUND"}},
+        {"TRANSACTION",std::vector<QString>() = {"TRANSACTION", "TAPAHTUMA", "TRANSAKTION"}},
+        {"TOTAL",std::vector<QString>() = {"TOTAL", "YHTEENSÄ", "SAMMANLAGT"}},
+        {"",std::vector<QString>() = {"", "", ""}},
+        {"",std::vector<QString>() = {"", "", ""}},
+        {"",std::vector<QString>() = {"", "", ""}},
+        {"",std::vector<QString>() = {"", "", ""}},
+        {"",std::vector<QString>() = {"", "", ""}},
+        {"",std::vector<QString>() = {"", "", ""}}
+
+    };
+
 };
 
 #endif // KUITTI_H
